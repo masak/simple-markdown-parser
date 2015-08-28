15f2a3e295bf0ab0b7e0b48e2bfefbcabfdc41d3
Carl Masak <cmasak@gmail.com>
2015-08-28 19:46:52 +0200

    handle more HTML blocks

--- lib/Markdown.pm
+++ lib/Markdown.pm
@@ -185,7 +185,7 @@ our sub to_html($input) {
             @elements.push: AtxHeader.new(:$contents);
             next LINE;
         }
-        elsif $line ~~ /^ '<dl>' $/ {   # XXX: generalize
+        elsif $line ~~ /^ '<dl>' | '<pre>' / {   # XXX: generalize
             my $contents = $line;
             @elements.push: HtmlBlock.new(:$contents);
             $eating_html = True;
